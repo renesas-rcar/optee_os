59149275c362b35ae36c4907f5e7a9466f92463b
Toshiyuki Ogasahara <toshiyuki.ogasahara.bo@hitachi.com>
2020-11-30 16:52:40 +0900

    plat-rcar: Fix the judgment of first smc in tee_entr_fast()
    Add missing '&' to the judgment.
    
    Signed-off-by: Toshiyuki Ogasahara <toshiyuki.ogasahara.bo@hitachi.com>
    Signed-off-by: Yoshifumi Hosoya <yoshifumi.hosoya.wj@renesas.com>

--- core/arch/arm/plat-rcar/main.c
+++ core/arch/arm/plat-rcar/main.c
@@ -47,7 +47,7 @@ void tee_entry_fast(struct thread_smc_args *args)
 {
 	DMSG("IN args->a0=0x%lX", args->a0);
 	if ((args->a0 == OPTEE_SMC_GET_SHM_CONFIG) &&
-	    (args->a1 == SMC_RCAR_CMD) &
+	    (args->a1 == SMC_RCAR_CMD) &&
 	    (args->a2 == START_DLOG_OUTPUT)) {
 		is_normal_world_initialized = 1;
 		DMSG("Normal World was initialized");
