d4af8b64fe6014107b39fb62427ed609cf0fd888
Toshiyuki Ogasahara <toshiyuki.ogasahara.bo@hitachi.com>
2021-10-15 16:44:29 +0900

    plat-rcar_gen4: Add memory map for S4
    Add the memory map definition required for S4 operation.
    
    Signed-off-by: Tsukasa Kawaguchi <tsukasa.kawaguchi.aw@hitachi.com>
    Signed-off-by: Toshiyuki Ogasahara <toshiyuki.ogasahara.bo@hitachi.com>
    Signed-off-by: Yoshifumi Hosoya <yoshifumi.hosoya.wj@renesas.com>
    Signed-off-by: Dien Pham <dien.pham.ry@renesas.com>

--- core/arch/arm/plat-rcar_gen4/platform_config.h
+++ core/arch/arm/plat-rcar_gen4/platform_config.h
@@ -25,6 +25,9 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
+/*
+ * Copyright (c) 2021, Renesas Electronics Corporation.
+ */
 
 #ifndef PLATFORM_CONFIG_H
 #define PLATFORM_CONFIG_H
@@ -32,16 +35,54 @@
 /* Make stacks aligned to data cache line length */
 #define STACK_ALIGNMENT		64
 
-#define GIC_BASE		0xF1000000
-#define GICC_BASE		0xF1020000
-#define GICD_BASE		0xF1010000
+#define GICC_BASE		0xF1060000U
+#define GICD_BASE		0xF1000000U
 
-#define CONSOLE_UART_BASE	0xE6E88000
+/*
+ * Last part of DRAM is reserved as secure dram, note that the last 2MiB
+ * of DRAM0 is used by SCP dor DDR retraining.
+ */
+#define TZDRAM_BASE		(0x44000000U)
 
-#define PRR_BASE		0xFFF00000
-#define PRR_OFFSET		0x44
-#define PRR_PRODUCT_M3W		0x5200
-#define PRR_PRODUCT_MASK	0xFF00
+/*
+ * Should be
+ * #define TZDRAM_SIZE		0x00FF8000
+ * but is smaller due to SECTION_SIZE alignment, can be fixed once
+ * OP-TEE OS is mapped using small pages instead.
+ */
+#define TZDRAM_SIZE		(0x02400000U)
+
+#define TEE_RAM_VA_SIZE		(1024 * 1024 * 3)
+
+#define TEE_RAM_START		(0x44100000)	/* TEE RAM address	*/
+#define TEE_RAM_PH_SIZE		(0x00300000U)	/* TEE RAM size		*/
+
+#define TA_RAM_START		(0x44400000U)	/* TA RAM address	*/
+#define TA_RAM_SIZE		(0x01800000U)	/* TA RAM size		*/
+
+#define TEE_SHMEM_START		(0x47E00000U)	/* Share Memory address	*/
+#define TEE_SHMEM_SIZE		(0x00100000U)	/* Share Memory size	*/
+
+#define OPTEE_LOG_BASE		(0x45E00000U)	/* OP-TEE Log Area address */
+#define OPTEE_LOG_NS_BASE	(0x47FEC000U)	/* OP-TEE Log Area NS addr */
+#define OPTEE_LOG_NS_SIZE	(0x00014000U)   /* OP-TEE Log Area NS size */
+
+#define TA_VERIFICATION_BASE	(0x45C00000U)	/* TA area for verification */
+#define TA_VERIFICATION_SIZE	(0x00100000U)	/* TA verification size */
+#define CRYPTOENGINE_WORK_BASE	(0x46000000U)	/* Crypto Enegine Work area */
+
+#define NONCACHE_WORK_BASE	(0x45F00000U)	/* Non Cache Area address */
+#define NONCACHE_WORK_SIZE	(0x00100000U)	/* Non Cache Area Size */
+
+#define LIFEC_BASE		(0xE6110000U)	/* Life Cycle address	*/
+#define RST_BASE		(0xE6160000U)	/* Reset address	*/
+#define CE_BASE			(0xE6600000U)	/* Crypto Engine address */
+#define RPC_BASE		(0xEE200000U)	/* RPC address		*/
+#define PRR_BASE		(0xFFF00000U)	/* Product Register address */
+#define RPC_ADDR_MAP_BASE	(0x08000000U)	/* RPC Internal address	*/
+#define RPC_ADDR_MAP_SIZE	(0x04000000U)	/* RPC Address Map size */
+
+#define MEM_SECTION_SIZE	(0x00100000U)
 
 #if defined(PLATFORM_FLAVOR_salvator_h3)
 #define NSEC_DDR_0_BASE		0x47E00000
@@ -83,8 +124,55 @@
 
 #endif
 
-/* Full GlobalPlatform test suite requires TEE_SHMEM_SIZE to be at least 2MB */
-#define TEE_SHMEM_START		(TZDRAM_BASE + TZDRAM_SIZE)
-#define TEE_SHMEM_SIZE		0x100000
+#define DEVICE1_PA_BASE		ROUNDDOWN(CE_BASE, CORE_MMU_PGDIR_SIZE)
+#define DEVICE1_SIZE		(MEM_SECTION_SIZE * 2)
+#define DEVICE1_TYPE		MEM_AREA_IO_SEC
+
+#define DEVICE2_PA_BASE		ROUNDDOWN(GICD_BASE, CORE_MMU_PGDIR_SIZE)
+#define DEVICE2_SIZE		(MEM_SECTION_SIZE)
+#define DEVICE2_TYPE		MEM_AREA_IO_SEC
+
+/* LOG Area for Secure World */
+#define MEMORY1_BASE		ROUNDDOWN(OPTEE_LOG_BASE, MEM_SECTION_SIZE)
+#define MEMORY1_SIZE		(MEM_SECTION_SIZE)
+#define MEMORY1_TYPE		MEM_AREA_IO_SEC
+
+/* LOG Area for Normal World */
+#define MEMORY2_BASE		(OPTEE_LOG_NS_BASE)
+#define MEMORY2_SIZE		(OPTEE_LOG_NS_SIZE)
+#define MEMORY2_TYPE		MEM_AREA_RAM_NSEC
+
+/* Crypto Engine Work area */
+#define MEMORY3_BASE		ROUNDDOWN(CRYPTOENGINE_WORK_BASE, \
+					MEM_SECTION_SIZE)
+#define MEMORY3_SIZE		(MEM_SECTION_SIZE)
+#define MEMORY3_TYPE		MEM_AREA_RAM_SEC
+
+/* TA area for verification */
+#define MEMORY4_BASE		ROUNDDOWN(TA_VERIFICATION_BASE, \
+					MEM_SECTION_SIZE)
+#define MEMORY4_SIZE		(TA_VERIFICATION_SIZE)
+#define MEMORY4_TYPE		MEM_AREA_IO_SEC
+
+/* Product Register (PRR) */
+#define MEMORY5_BASE		ROUNDDOWN(PRR_BASE, MEM_SECTION_SIZE)
+#define MEMORY5_SIZE		(MEM_SECTION_SIZE)
+#define MEMORY5_TYPE		MEM_AREA_IO_SEC
+
+/* Non Cache Stack Area */
+#define MEMORY6_BASE		ROUNDDOWN(NONCACHE_WORK_BASE, \
+					NONCACHE_WORK_SIZE)
+#define MEMORY6_SIZE		(MEM_SECTION_SIZE)
+#define MEMORY6_TYPE		MEM_AREA_IO_SEC
+
+/* SPI Multi I/O Bus Controller (RPC) */
+#define MEMORY7_BASE		ROUNDDOWN(RPC_BASE, MEM_SECTION_SIZE)
+#define MEMORY7_SIZE		(MEM_SECTION_SIZE)
+#define MEMORY7_TYPE		MEM_AREA_IO_SEC
+
+/* RPC Address Map */
+#define MEMORY8_BASE		ROUNDDOWN(RPC_ADDR_MAP_BASE, MEM_SECTION_SIZE)
+#define MEMORY8_SIZE		(RPC_ADDR_MAP_SIZE)
+#define MEMORY8_TYPE		MEM_AREA_IO_SEC
 
 #endif /*PLATFORM_CONFIG_H*/
