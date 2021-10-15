271ce43b7c5fc634b41c4f41fdecd91c0c06b573
Toshiyuki Ogasahara <toshiyuki.ogasahara.bo@hitachi.com>
2021-10-15 16:43:11 +0900

    plat-rcar_gen4: First commit for S4
    Add plat-rcar_gen4 folder.
    
    Signed-off-by: Tsukasa Kawaguchi <tsukasa.kawaguchi.aw@hitachi.com>
    Signed-off-by: Toshiyuki Ogasahara <toshiyuki.ogasahara.bo@hitachi.com>
    Signed-off-by: Yoshifumi Hosoya <yoshifumi.hosoya.wj@renesas.com>
    Signed-off-by: Dien Pham <dien.pham.ry@renesas.com>

---
+++ core/arch/arm/plat-rcar_gen4/rcar_version.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (c) 2021, Renesas Electronics Corporation.
+ */
+
+#ifndef RCAR_VERSION_H
+#define RCAR_VERSION_H
+
+#include <stdint.h>
+
+#define VERSION_OF_RENESAS	"0.2.0"
+
+#endif /* RCAR_VERSION_H */
