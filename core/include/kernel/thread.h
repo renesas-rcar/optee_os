b566857938e8b205382039617649fd50a75dc124
Takuya Sakata <takuya.sakata.wz@bp.renesas.com>
2022-03-04 20:21:25 +0900

    core: Add a command to wait for SPI/HyperFlash Driver processing completion
    Add a command to thread_hw_wait_cmd function that
    waits for SPI/HyperFlash driver processing to complete.
    
    Signed-off-by: Tsukasa Kawaguchi <tsukasa.kawaguchi.aw@hitachi.com>
    Signed-off-by: Toshiyuki Ogasahara <toshiyuki.ogasahara.bo@hitachi.com>
    Signed-off-by: Yoshifumi Hosoya <yoshifumi.hosoya.wj@renesas.com>
    Signed-off-by: Dien Pham <dien.pham.ry@renesas.com>

--- core/include/kernel/thread.h
+++ core/include/kernel/thread.h
@@ -394,6 +394,11 @@ void *thread_rpc_shm_cache_alloc(enum thread_shm_cache_user user,
 				 enum thread_shm_type shm_type,
 				 size_t size, struct mobj **mobj);
 
+#ifdef PLATFORM_rcar_gen4
+TEE_Result thread_hw_wait_cmd(const TEE_Time *base_time, uint32_t timeout,
+				uint32_t wait, uint32_t delay);
+#endif
+
 #endif /*__ASSEMBLER__*/
 
 #endif /*__KERNEL_THREAD_H*/
