4a675a28a8ca74028666721d217098852f4f7d3a
Anh Vo <anh.vo.vx@renesas.com>
2025-02-06 14:32:03 +0700

    compiler: Change __pure attribute to avoid redefinition with QNX compiler
    Fix ERROR
    "make[1]: Entering directory '/data/anhvo2/1_Work-dir/V4H_OPTEE/5_Repo/_from-QNX-team/tee_qnx_010725/source/optee_test/host/xtest'
      CC      /data/anhvo2/1_Work-dir/V4H_OPTEE/5_Repo/_from-QNX-team/tee_qnx_010725/source/optee_test/host/xtest/regression_1000.c
    In file included from /data/anhvo2/1_Work-dir/V4H_OPTEE/5_Repo/optee_os/out/arm-plat-rcar_gen4/export-ta_arm64/host_include/tee_api_types.h:10,
                     from /data/anhvo2/1_Work-dir/V4H_OPTEE/5_Repo/optee_os/out/arm-plat-rcar_gen4/export-ta_arm64/host_include/signed_hdr.h:10,
                     from /data/anhvo2/1_Work-dir/V4H_OPTEE/5_Repo/_from-QNX-team/tee_qnx_010725/source/optee_test/host/xtest/regression_1000.c:23:
    /data/anhvo2/1_Work-dir/V4H_OPTEE/5_Repo/optee_os/out/arm-plat-rcar_gen4/export-ta_arm64/host_include/compiler.h:27: error: "__pure" redefined [-Werror]
     #define __pure  __attribute__((pure))
    
    In file included from /shsv/SS2/RSS1/qnx/toolchain/qnx710_base/target/qnx7/usr/include/sys/compiler_gnu.h:64,
                     from /shsv/SS2/RSS1/qnx/toolchain/qnx710_base/target/qnx7/usr/include/sys/platform.h:37,
                     from /shsv/SS2/RSS1/qnx/toolchain/qnx710_base/target/qnx7/usr/include/errno.h:23,
                     from /data/anhvo2/1_Work-dir/V4H_OPTEE/5_Repo/_from-QNX-team/tee_qnx_010725/source/optee_test/host/xtest/regression_1000.c:7:
    /shsv/SS2/RSS1/qnx/toolchain/qnx710_base/target/qnx7/usr/include/sys/cdefs.h:149: note: this is the location of the previous definition
     # define __pure __attribute__((__pure__))
    
    cc1: all warnings being treated as errors
    "
    
    Signed-off-by: Anh Vo <anh.vo.vx@renesas.com>
    Signed-off-by: Tu Tran <tu.tran.wz@renesas.com>

--- lib/libutils/ext/include/compiler.h
+++ lib/libutils/ext/include/compiler.h
@@ -24,7 +24,9 @@
 #ifndef __noreturn
 #define __noreturn	__attribute__((__noreturn__))
 #endif
+#ifndef __pure
 #define __pure		__attribute__((pure))
+#endif
 #define __aligned(x)	__attribute__((aligned(x)))
 #define __printf(a, b)	__attribute__((format(printf, a, b)))
 #define __noinline	__attribute__((noinline))
