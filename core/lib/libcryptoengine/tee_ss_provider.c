6a35f7b9bbfd2f8f059511e2ecc46fdb389985dd
Nhi Nguyen <nhi.nguyen.kx@renesas.com>
2025-04-28 11:32:16 +0700

    plat-rcar: ss_provider: Add update the local ftmn_func_arg structure
    When enbale the configuration: CFG_HW_CRYPTO_ENGINE=y,
    Xtest has the failure of panic when check the respond value
    of the callee function in opening the TA (verify the signed header
    of TA process) following new method of fault mitigation of upstream.
    
    In case of HW engine test, we need to update the value of the local
    structure ftmn_func_arg supplied by caller, which calls the FTMN_CALLEE_DONE(res)
    API of fault mitigation feature after verifying the signature of TA in SS63
    secure driver.
    
    =================================================================
    Call FTMN_CALLEE_DONE(res) (with res is the returning value of callee),
    which includes important points as:
      arg->hash ^= my_hash;
      arg->res = arg->hash ^ res;
    =================================================================
    
    After updating the local ftmn_func_arg structure as above
    The test case will execture successfully
    
    Signed-off-by: Nhi Nguyen <nhi.nguyen.kx@renesas.com>

--- core/lib/libcryptoengine/tee_ss_provider.c
+++ core/lib/libcryptoengine/tee_ss_provider.c
@@ -30,6 +30,7 @@
 #include "include_secure/secure_key_gen.h"
 #include "rcar_mutex.h"
 #include "rcar_common.h"
+#include <fault_mitigation.h>
 
 
 #ifdef CFG_CRYPT_ENABLE_CEPKA
@@ -3022,6 +3023,9 @@ TEE_Result crypto_hw_acipher_rsassa_verify(uint32_t algo,
 		}
 		PROV_DMSG("Result: crys_res=0x%08x -> res=0x%08x\n",crys_res,res);
 	}
+
+	FTMN_CALLEE_DONE(res);
+
 	rcar_nex_mutex_unlock(&secure_asymm_mutex);
 
 	ss_free((void *)userContext_ptr);
