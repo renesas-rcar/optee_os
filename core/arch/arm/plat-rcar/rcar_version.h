96d1b7362f4b4156470d73ff68378fac8010cbdb
Nhi Nguyen <nhi.nguyen.kx@renesas.com>
2025-05-15 11:32:32 +0700

    plat-rcar: Update optee_os Rev3.0.6
    Signed-off-by: Nhi Nguyen <nhi.nguyen.kx@renesas.com>

--- core/arch/arm/plat-rcar/rcar_version.h
+++ core/arch/arm/plat-rcar/rcar_version.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: BSD-2-Clause */
 /*
- * Copyright (c) 2015-2024, Renesas Electronics Corporation
+ * Copyright (c) 2015-2025, Renesas Electronics Corporation
  */
 
 #ifndef RCAR_VERSION_H
@@ -8,7 +8,7 @@
 
 #include <stdint.h>
 
-#define VERSION_OF_RENESAS	"3.0.5"
+#define VERSION_OF_RENESAS	"3.0.6"
 
 extern const int8_t version_of_renesas[];
 
