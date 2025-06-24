65e321cc39536e4f888166a13e17b29da3c044e4
Dien Pham <dien.pham.ry@renesas.com>
2025-06-24 21:55:45 +0700

    Export spinlock for ss6.3 driver
    Signed-off-by: Tu Tran <tu.tran.wz@renesas.com>
    Signed-off-by: Dien Pham <dien.pham.ry@renesas.com>

--- core/include/kernel/spinlock.h
+++ core/include/kernel/spinlock.h
@@ -16,6 +16,10 @@
 #include <kernel/thread.h>
 #include <stdbool.h>
 
+#ifdef CFG_CRYPT_HW_CRYPTOENGINE
+extern unsigned int spin_lock __nex_data;
+#endif
+
 #ifdef CFG_TEE_CORE_DEBUG
 void spinlock_count_incr(void);
 void spinlock_count_decr(void);
