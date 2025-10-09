8625a2e095d6415f785ab92b332460a9aca40a02
Dien Pham <dien.pham.ry@renesas.com>
2025-10-09 21:57:15 +0700

    plat_rcar: Provide same interface of phys_to_virt() for optee 3.13 and 3.22
    This function exports same interface to map physical address
    to logical address between optee version 3.13 and
    version as 3.22 or later.
    
    This is intended for using same hardware secure engine
    source code for both optee v3.13 and v3.22 later.
    
    Signed-off-by: Dien Pham <dien.pham.ry@renesas.com>

--- core/arch/arm/plat-rcar/main.c
+++ core/arch/arm/plat-rcar/main.c
@@ -24,6 +24,7 @@
 #include <mm/core_mmu.h>
 #include <console.h>
 #include <io.h>
+#include <types_ext.h>
 
 #include <sm/optee_smc.h>
 #include <kernel/spinlock.h>
@@ -271,4 +272,12 @@ void console_init(void)
 	register_serial_console(&console_data.chip);
 	IMSG("Init SCIF driver before mmu enable");
 #endif
+}
+
+/** This function exports same interface to map physical address to logical address
+ * between version 3.13 and later version as 3.22 and later.
+ */
+void *rcar_phys_to_virt(paddr_t pa, enum teecore_memtypes m, __maybe_unused size_t len)
+{
+	return phys_to_virt(pa, m);
 }
